0e1cf4105ead7f1c4bcafc8e89eed2f110757acd
SubhamSwain008 <subhamswain8456@gmail.com>
2025-11-15 01:11:29 +0530

    admin pannel progress

--- backend/src/api/contoller/Admin/admin.panel.controllers.js
+++ backend/src/api/contoller/Admin/admin.panel.controllers.js
@@ -24,7 +24,10 @@ export const verifyUsers=async(req,res)=>{
    try{
       const userToUpdate=await Merchants.findOne({email});
       if(!userToUpdate) return res.status(400).json({message:"user not found"});
-      
+      userToUpdate.isApprovedByAdmin=true;
+      userToUpdate.save();
+      return res.status(200).json({message:"user approved by admin"});
+
    }catch(e){
       console.log(e);
       return res.status(400).json({message:"can not verify user due to internal errors"})
@@ -32,3 +35,16 @@ export const verifyUsers=async(req,res)=>{
 
    
 }
+
+export const showAllMerchants=async(req,res)=>{
+   if(!req.user.isAdmin) return res.status(400).json({message:"you are not admin!"});
+   try{
+   const users= await Merchants.find({isApprovedByAdmin:true ,isAdmin: false}, "fullName email phoneNumber");
+   if(!users) return res.status(404).json({message:"no user found"});
+
+   return res.status(200).json({message:"users found",users:users});
+   }
+   catch(e){
+      return res.status(200).json({message:"failed to find merchants due to internal errors"});
+   }
+   }
--- backend/src/api/routes/admin/admin.panel.routes.js
+++ backend/src/api/routes/admin/admin.panel.routes.js
@@ -1,6 +1,7 @@
-import { showUnverifiedMerchants } from "../../contoller/Admin/admin.panel.controllers.js";
+import { showUnverifiedMerchants, verifyUsers ,showAllMerchants } from "../../contoller/Admin/admin.panel.controllers.js";
 import { protectedRoute } from "../../../middleware/jwt.middleware.js";
 import express from "express";
+
 
 
 const adminRouter= express.Router();
@@ -10,6 +11,8 @@ adminRouter.get("/running",(req,res)=>{
     res.json({message:"admin route is running"});
 });
 adminRouter.post("/show-unverified",protectedRoute,showUnverifiedMerchants);
+adminRouter.post("/show-verified",protectedRoute,showAllMerchants);
+adminRouter.post("/verify-users",protectedRoute,verifyUsers);
 
 
 export default adminRouter;
